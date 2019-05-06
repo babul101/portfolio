d12a9729fd84fe2494453d62bf1c5f69f9fad89e
babul101 <babul101@gmail.com>
2019-05-06 16:41:51 +0000

    Debugged Blog listing

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,18 +1,24 @@
+import React from 'react';
 import Header from "../shared/Header";
+import Head from 'next/head';
 
 const BaseLayout = (props) => {
     const {className,children,isAuthenticated} = props;
     const headerType = props.headerType || 'default';
   return (
-    <div className="layout-container">
-      {/* { headerType === 'index'&& <Header className='port-nav-index' isAuthenticated={isAuthenticated} />} */}
-      {/* { headerType === 'default' && <Header className='port-nav-default' isAuthenticated={isAuthenticated} />} */}
-      <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated} />}
-      <main className={`cover ${className}`}>
-        <div className="wrapper">{children}</div>
-      </main>
-    </div>
+    <React.Fragment>
+    <Head>
+    <title>Babul Deb</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
+    </Head>
+      <div className="layout-container">
+      <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated} />
+        <main className={`cover ${className}`}>
+          <div className="wrapper">{children}</div>
+        </main>
+      </div>
+    </React.Fragment>
   );
-};
+}
 
 export default BaseLayout;
