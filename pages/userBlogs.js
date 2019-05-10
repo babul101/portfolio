965bb5b6d1cf1e96deae3a513fbbd47a83cc3a73
babul101 <babul101@gmail.com>
2019-05-10 06:15:23 +0000

    Displaying User Blogs

--- pages/userBlogs.js
+++ pages/userBlogs.js
@@ -4,6 +4,7 @@ import BasePage from '../components/BasePage';
 import withAuth from '../components/hoc/withAuth';
 import {Container,Row,Col} from 'reactstrap';
 import {getUserBlogs} from '../actions';
+import {Link} from '../routes';
 
  class UserBlogs extends Component {
 
@@ -17,9 +18,36 @@ import {getUserBlogs} from '../actions';
     }
     return {blogs};
   }
+
+  seperateBlogs(blogs) {
+    const published = [];
+    const drafts = [];
+
+    blogs.forEach((blog)=> {
+      blog.status === 'draft' ? drafts.push(blog) : published.push(blog);
+    })
+    return {published,drafts};
+  }
+
+  renderBlogs(blogs) {
+    return (
+      <ul className='user-blogs-list'>
+        {
+          blogs.map((blog,index)=>(
+            <li key={index}>
+              <Link route={`/blogs/${blog._id}/edit`}>
+                <a>{blog.title}</a>
+              </Link>
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
+
   render() {
     const {blogs} = this.props;
-    
+    const {published,drafts} = this.seperateBlogs(blogs);
     return (
         <BaseLayout  {...this.props.auth} headerType={'landing'}>
         <div className="masthead" style={{"backgroundImage": "url('/static/images/home-bg.jpg')"}}>
@@ -37,8 +65,14 @@ import {getUserBlogs} from '../actions';
         </div>
         <BasePage className="blog-user-page">
           <Row>
-            <Col md='6' className='mx-auto text-center'>Published Blogs</Col>
-            <Col md='6' className='mx-auto text-center'>Draft Blogs</Col>
+            <Col md='6' className='mx-auto text-center'>
+            <h2 className='blog-status-title'>Published Blogs</h2>
+            {this.renderBlogs(published)}
+            </Col>
+            <Col md='6' className='mx-auto text-center'>
+            <h2 className='blog-status-title'>Draft Blogs</h2>
+            {this.renderBlogs(drafts)}
+            </Col>
           </Row>
         </BasePage>
       </BaseLayout>
